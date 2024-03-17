4f2d447ecabed4a81d4c689c13dacc8c3577975d
Sofia <sofiamint1995@gmail.com>
2024-03-17 12:21:45 +0400

    whole lil project

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const todoRoutes = require('./routes/todos')
+
+const PORT = process.env.PORT || 3000; //what port 2 use - from system variable OR 3000
+
+const app = express(); //creating application object
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine) //engine for pages rendering
+app.set('view engine', 'hbs')
+app.set('views', 'views') //setting a folder for views
+
+app.use(express.urlencoded({ extended:true })) //adding the middleware
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(todoRoutes)
+
+async function start() {
+    try {
+        await mongoose.connect('mongodb+srv://sonya:143143@cluster0.pahwmns.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0', { //connect to db
+            useNewUrlParcer: true,
+            useFindAndModify: false
+        })
+        app.listen(PORT, () => {
+            console.log('Server has been started!')    
+        }) //starting the server
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
